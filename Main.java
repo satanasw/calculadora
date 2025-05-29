644d7ac9c10f4d638bd6034c8412e1c4943d6200
Alvaro Marcos <alvaro.marsan.4@educa.jcyl.es>
2025-05-29 13:34:31 +0200

    comentario2

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    // <p>Take my clothes baby</p>
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -12,6 +13,6 @@ public class Main {
             System.out.println("i = " + i);
         }
 
-        <p>Take my clothes baby</p>
+        
     }
 }
