1683aef26edc5098c435c3d6462a8177c9be2186
Alvaro Marcos <alvaro.marsan.4@educa.jcyl.es>
2025-05-29 13:35:43 +0200

    cambio sout

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    // <p>Take my clothes baby</p>
+    
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -12,7 +12,7 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
-
-        
-    }
+        System.out.println("Hola walid");
+        // <p>Take my clothes baby</p>
+        }
 }
